76a01aa4180342b1b9bbba721d9f08a61e798c58
Davi <davi.galdino08@gmail.com>
2022-10-03 14:42:43 -0300

    RegisterStudents 2/3

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import { RegisterStudent } from './pages/RegisterStudent';
+import { StudentForms } from './pages/StudentForms';
+
+function App() {
+  return (
+    <div className="App">
+      <StudentForms/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/StudentForms/index.jsx
@@ -0,0 +1,112 @@
+import './styles.css';
+import { SideBlue } from '../../components/SideBlue';
+import { FormControlLabel, Radio, RadioGroup, FormLabel, FormControl, TextField, Button } from '@mui/material';
+
+export const StudentForms = () => {
+    return(
+        <div className="register-container">
+
+            <SideBlue/>
+
+            <div className="register">
+
+                <form action="#" id="info-forms">
+                    <div className="register-header">
+                        <div className="back-button">
+                            <a href="">Voltar</a>
+                        </div>
+                        <div className="title">
+                            <h1>Cadastro</h1>
+                            <h2>Perfil Sócioeconômico</h2>
+                        </div>
+                    </div>
+
+                    <div className="jobs-area">
+                        <FormControl>
+                            <FormLabel id="jobs-area-label">Você trabalha/estagia atualmente?</FormLabel>
+                            <RadioGroup
+                                row
+                                aria-aria-aria-labelledby='jobs-area-label'
+                                name='jobs-area'
+                            >
+                                <FormControlLabel value="sim" control={<Radio/>} label="Sim"/>
+                                <FormControlLabel value="nao" control={<Radio/>} label="Não"/>
+                            </RadioGroup>
+                        </FormControl>
+                    </div>
+
+                    <div className="jobs-location">  
+                        <FormLabel id="jobs-location-label">Se sim, onde?</FormLabel>
+                        <TextField
+                            sx={{width: '358px', color: '#2B2C33'}}
+                        />
+                    </div>
+
+                    <div className="jobs-function">
+                        <FormControl>
+                            <FormLabel id="jobs-function-label">É uma empresa de tecnologia?</FormLabel>
+                            <RadioGroup
+                                row
+                                aria-aria-aria-labelledby='job-area-label'
+                                name='job-area'   
+                            >
+                                <FormControlLabel value="sim" control={<Radio/>} label="Sim"/>
+                                <FormControlLabel value="nao" control={<Radio/>} label="Não"/>
+                            </RadioGroup>
+                        </FormControl>
+                    </div>
+
+                    <div className="jobs-section">
+                        <FormLabel id="jobs-section-label">Se sim, trabalha no setor de tecnologia dela?</FormLabel>
+                        <RadioGroup
+                            row
+                            aria-aria-aria-labelledby='job-area-label'
+                            name='job-area'   
+                        >
+                            <FormControlLabel value="sim" control={<Radio/>} label="Sim"/>
+                            <FormControlLabel value="nao" control={<Radio/>} label="Não"/>
+                        </RadioGroup>
+                    </div>
+
+                    <div className="jobs-hours">
+                        <FormLabel id="jobs-hours-label">Em qual horário?</FormLabel>
+                        <TextField
+                            sx={{width: '358px', color: '#2B2C33'}}
+                        />
+                    </div>
+
+                    <div className="residence-area">
+                        <FormLabel id="residence-area-label">Você já participou da Residência Onboard?</FormLabel>
+
+                        <RadioGroup
+                            row
+                            aria-aria-aria-labelledby='job-area-label'
+                            name='job-area'   
+                        >
+                            <FormControlLabel value="sim" control={<Radio/>} label="Sim"/>
+                            <FormControlLabel value="nao" control={<Radio/>} label="Não"/>
+                        </RadioGroup>
+                    </div>
+
+                    <div className="button-advance">
+                        <Button 
+                            variant={"contained"}
+                            sx={{
+                                width: '210px',
+                                height: '50px',
+                                borderRadius: '10px',
+                                fontFamily: 'Space Grotesk',
+                                fontSize: '16px',
+                                weight: '700',
+                                backgroundColor: '#000000'
+                            }}
+                        >
+                            Avançar
+                        </Button>
+                    </div>
+
+                </form>
+            </div>
+        </div>
+    )
+}
